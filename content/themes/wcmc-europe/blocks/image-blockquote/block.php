38a07b2f0b0c32b6dbe5c15f71c90b302e2dc2b8
nym1k <sleeping.stars@gmail.com>
2021-01-22 18:05:42 +0000

    fix: fixing image on image-blockquote comp

--- content/themes/wcmc-europe/blocks/image-blockquote/block.php
+++ content/themes/wcmc-europe/blocks/image-blockquote/block.php
@@ -1,6 +1,6 @@
 <?php
   /*
-    Featured Link Block
+    Image Blockquote Block
     Created by UNEP-WCMC
     With Genesis Custom Blocks for Gutenberg - https://wpengine.co.uk/genesis-custom-blocks/
   */
@@ -8,26 +8,16 @@
   // Variables
   $text = block_field( 'text', false );
 
-  $link_text = block_field( 'link-text', false );
-  $link_url = block_field( 'link-url', false );
-  $external = block_field( 'external', false );
+  $overlay_opacity = block_field( 'over-opacity', false );
 
-  $image = block_field( 'image', false );
+  $image = block_field( 'background-image', false );
   $image_url = wp_get_attachment_image_src( $image, 'full-size' )[0];
 ?>
 
-<div class="featured-link">
-  <div class="featured-link__columns">
-    <div class="featured-link__column">
-      <div class="featured-link__image-wrapper">
-        <img class="featured-link__image" src="<?php echo $image_url ?>" alt="">
-      </div>
-    </div>
-    <div class="featured-link__column">
-      <div class="featured-link__content">
-        <h3 class="featured-link__title"><?php echo $text; ?></h3>
-        <a class="featured-link__link" href="<?php echo $link_url; ?>"><?php echo $link_text; ?></a>
-      </div>
-    </div>
+<div class="image-blockquote">
+  <img src="<?php echo $image_url; ?>" alt="<?php echo $text; ?>" class="image-blockquote_background-image"
+  style="opacity: <?php echo $overlay_opacity; ?>">
+  <div class="image-blockquote_content">
+    <h4 class="image-blockquote_text"><?php echo $text; ?></h4>
   </div>
 </div>
