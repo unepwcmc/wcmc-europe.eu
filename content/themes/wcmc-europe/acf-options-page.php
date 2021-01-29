07aee60130ec9aa8dfe59c546ad881e1a94067ee
nym1k <sleeping.stars@gmail.com>
2021-01-29 14:24:10 +0000

    feat: adding fullwidth page template, and ACF options page

---
+++ content/themes/wcmc-europe/acf-options-page.php
@@ -0,0 +1,18 @@
+<?php
+  if( function_exists('acf_add_options_page') ) {
+
+  	acf_add_options_page(array(
+  		'page_title' 	=> 'Site General Settings',
+  		'menu_title'	=> 'Site Settings',
+  		'menu_slug' 	=> 'site-general-settings',
+  		'capability'	=> 'edit_posts',
+  		'redirect'		=> false
+  	));
+
+  	acf_add_options_sub_page(array(
+  		'page_title' 	=> 'Site Contact Settings',
+  		'menu_title'	=> 'Contact',
+  		'parent_slug'	=> 'site-general-settings',
+  	));
+
+  }
---
+++ content/themes/wcmc-europe/page-full-width.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying full width page
+ *
+ * Template Name: Full Width
+ *
+ */
+
+get_header(); ?>
+
+<div class="lyt-Container">
+	<div class="lyt-Container_Inner">
+		<div class="lyt-Container_Body">
+			<section class="lyt-Primary">
+				<div class="lyt-Primary_Body">
+					<?php get_template_part( 'template-parts/content/content', 'none' ); ?>
+				</div>
+			</section>
+		</div>
+	</div>
+</div>
+
+<?php get_footer();
