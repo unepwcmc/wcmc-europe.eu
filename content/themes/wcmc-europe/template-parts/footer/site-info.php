8e2b937d2a53d6ed8532229227cdf814f3a3d768
Jonathan Feist <jonathan.feist@unep-wcmc.org>
2021-02-03 16:23:29 +0000

    refactor: remove unused footer file

--- content/themes/wcmc-europe/template-parts/footer/site-info.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Displays footer site info
- **/
-?>
-<img class="ft-Footer_Image" src="<?php echo get_stylesheet_directory_uri() . '/dist/img/arcadia-logo.png' ?>" alt="Arcadia">
-<p class="ft-Footer_Text"><em>IUCN Urban Alliance is funded by the Arcadia Fund, a UK charity organisation founded by Lisbet Rausing and Professor Peter Baldwin. Established in 2001, the organisation provides grants on a worldwide basis. Its primary focus currently is preserving endangered culture, preserving endangered nature and providing open access.</em></p><!-- .site-info -->
