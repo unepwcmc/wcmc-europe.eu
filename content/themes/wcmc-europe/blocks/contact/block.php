817d59fee087e61bb5078d6337a4c9e54ab74d6e
nym1k <sleeping.stars@gmail.com>
2021-04-14 12:32:42 +0100

    feat: adding empty block structure for GCBs with no fields

---
+++ content/themes/wcmc-europe/blocks/contact/block.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Contact Form Block
+ * Created by UNEP-WCMC
+ * With Genesis Custom Blocks for Gutenberg - https://wpengine.co.uk/genesis-custom-blocks/
+ */
+
+ // No fields required
