fa6f17d487bfb9d3e4ce2c146161fb7702b971c5
nym1k <sleeping.stars@gmail.com>
2021-04-14 13:05:02 +0100

    feat: adding empty block strcuture for full width carousel

---
+++ content/themes/wcmc-europe/blocks/full-width-carousel/block.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Featured Link Block
+ * Created by UNEP-WCMC
+ * With Genesis Custom Blocks for Gutenberg - https://wpengine.co.uk/genesis-custom-blocks/
+ */
+
+  // TODO: Build Gutenberg version
+?>
