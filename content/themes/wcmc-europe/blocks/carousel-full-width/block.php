519b2fcc31b6b0fadbf77d066a8a9b603f1c4b3b
nym1k <sleeping.stars@gmail.com>
2022-06-10 10:37:32 +0100

    feat: gutenberg styles for full width carousel, adding extra mixins

--- content/themes/wcmc-europe/blocks/carousel-full-width/block.php
+++ content/themes/wcmc-europe/blocks/carousel-full-width/block.php
@@ -1,10 +1,54 @@
 <?php
 
 /**
- * Featured Link Block
+ * Full Width Carousel Block
  * Created by UNEP-WCMC
- * With Genesis Custom Blocks for Gutenberg - https://wpengine.co.uk/genesis-custom-blocks/
+ * With Block Lab for Gutenberg - https://getblocklab.com/
  */
-
-  // TODO: Build Gutenberg version
 ?>
+
+<div class="carousel-full">
+
+  <?php if ( block_rows( 'cells' ) ) : ?>
+
+    <div class="carousel-full__cells">
+      <?php $index = 0; ?>
+      <?php while ( block_rows( 'cells' ) ) : block_row( 'cells' ); ?>
+        <?php if ($index < 1): ?>
+          <?php
+            $title = block_sub_value('title');
+            $label = block_sub_value('label');
+
+            $background_image = block_sub_value('imageId');
+            $background_image_url = wp_get_attachment_image_src($background_image, 'full-size')[0];
+          ?>
+          <div class="carousel-full__cell">
+            <div class="carousel-full__content">
+              <h3 class="carousel-full__label">
+                <?php echo $label; ?>
+              </h4>
+              <h4 class="carousel-full__title">
+                <?php echo $title; ?>
+              </h4>
+              <p class="carousel-full__link">
+                View more
+                <?php get_template_part( 'template-parts/icons/icon', 'arrow' ); ?>
+              </p>
+            </div>
+            <img
+              src="<?php echo $background_image_url; ?>"
+              alt="<?php echo $title; ?>"
+              class="carousel-full__background-image"
+            >
+          </div>
+          <?php $index++; ?>
+        <?php endif; ?>
+
+      <?php endwhile; ?>
+
+    </div>
+
+  <?php endif; ?>
+
+  <?php get_template_part( 'template-parts/carousel/faux', 'buttons' ); ?>
+</div>
